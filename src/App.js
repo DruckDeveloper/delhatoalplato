6ac09140dd47f6825ee10303cbcf900b5e7ba036
DruckDeveloper <druckdev@gmail.com>
2022-12-22 15:57:59 -0500

    React routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+//Import react router dom
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//Import pages 
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Products from './pages/Products'
+//Import components 
+import Sidebar from './components/Sidebar' 
+import Header from './components/Header' 
+import Footer from './components/Footer' 
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return <div className='overflow-hidden'>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}  />
+        <Route path='/product/:id' element={<Products />}  />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
+  </div>;
 };
 
 export default App;
