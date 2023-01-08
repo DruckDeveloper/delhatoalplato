e1f1117ae24f94a887d4e7e18d3f3e8ade5f957e
DruckDeveloper <druckdev@gmail.com>
2023-01-08 15:04:16 -0500

    embebed map added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import '../styles/MapStyles.css'
 
 function Map() {
   return (
-    <div class="mapouter">
-      <div class="gmap_canvas">
-        <iframe
-          className="gmap_iframe"
-          frameborder="0"
-          scrolling="no"
-          marginheight="0"
-          marginwidth="0"
-          src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=carnes del hato al plato calle 4 19 - 21 zipaquira cundinamarca colombia&amp;t=&amp;z=20&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">    
-        </iframe>
-      </div>
+    <div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.5313361908607!2d-74.00100504910813!3d5.017192240209697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e40719c97fce44d%3A0xac1d089fa99b7b9e!2sCarnes%20del%20Hato%20al%20Plato!5e0!3m2!1ses!2sco!4v1673194161735!5m2!1ses!2sco"
+        width="600"
+        height="450"
+        style={{border: 0}}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 }
